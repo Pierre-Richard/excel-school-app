75c416b67c1fa0061cbdd5fa8fe17f8ae34e27ea
Pierre-Richard <mayer_alexandre@yahoo.fr>
2026-04-14 12:13:44 +0200

    added UpdateStudentDto

--- backend/excel-school-app/DTOs/Student/UpdateStudentDto.cs
+++ backend/excel-school-app/DTOs/Student/UpdateStudentDto.cs
@@ -4,6 +4,12 @@ namespace excel_school_app.DTOs
 {
     public class UpdateStudentDto
     {
-        
+         public int UserId { get; set; }
+          public int ClasseId { get; set; }
+          public int ParentId { get; set; }
+          public string Name { get; set; } = string.Empty;
+          public string Firstname { get; set; } = string.Empty; 
+          public int StudentNumber { get; set; }
+          public DateTime BirthDate { get; set; } 
     }
 }
